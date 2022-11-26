7823c30e855a7b6dc05fdea4f4e76cc2fdfaedfc
Maxsot <sotnikov.maxim3007@gmail.com>
2022-11-26 03:15:45 -0800

    max_min

---
+++ first_task(max,min)/Program.cs
@@ -0,0 +1,19 @@
+﻿int numberA = 10;
+int numberB = 8;
+int max ;
+int min ;
+
+if(numberA > numberB)
+{
+    max = numberA;
+    min = numberB;
+}
+else 
+{
+    max = numberB;
+    min = numberA;
+}
+
+
+Console.WriteLine("max = " + max);
+Console.WriteLine("min = " + min);
