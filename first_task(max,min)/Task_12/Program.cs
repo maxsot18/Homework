c1964165d2184a0881b99843e6600b8e5c29cafb
Maxsot <sotnikov.maxim3007@gmail.com>
2022-11-26 07:07:39 -0800

    Changes at task 01

---
+++ first_task(max,min)/Task_12/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter a number to find max and min value ");
+string InputValue1 = Console.ReadLine();
+int N1 = Convert.ToInt32(InputValue1);
+
+Console.Write("Enter a number to find max and min value ");
+string InputValue2 = Console.ReadLine();
+int N2 = Convert.ToInt32(InputValue2);
+int max = N1;
+int min = N1;
+if(N1 < N2)
+{
+     max = N2;
+     min = N1;   
+}
+
+if(N1 > N2)
+{
+     max = N1;
+     min = N2;
+}
+Console.WriteLine("Max = " + max);
+Console.Write("Min = " + min);
