c2eff0c054df53412d0cf84d12495c0e4c379196
Maxsot <sotnikov.maxim3007@gmail.com>
2022-11-27 04:12:20 -0800

    Not unsolved task

---
+++ Task_04/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter a number ");
+string inputvalue = Console.ReadLine();
+int value = Convert.ToInt32(inputvalue);
+
+int i = 0;
+while(i < value)
+{
+    if(value[i] % 2 == 0)
+    {
+        Console.WriteLine(value[i]);
+    }
+   
+
+}
+
